a6ce3d0bbc21c067d109c4ddc28c1db4db4dd64b
Dista Reza <dista.reza@bertelsmann.de>
2022-05-16 07:02:04 +0800

    update

---
+++ main_amazon_price_tracker.py
@@ -0,0 +1,8 @@
+"""
+    Use BeautifulSoup to Scrape the Product Price
+
+    Email Alert When Price Below Preset Value
+"""
+
+
+# Find a product on Amazon that you want to track and get the product URL or just use the one I'm tracking.
